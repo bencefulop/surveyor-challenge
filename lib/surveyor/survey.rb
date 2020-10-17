744e4ce3f74ab5fb3190deea84f85be96aae6525
Bence Fulop <fbence90@gmail.com>
2020-10-17 18:08:54 +1100

    Add response method

--- lib/surveyor/survey.rb
+++ lib/surveyor/survey.rb
@@ -1,15 +1,20 @@
 module Surveyor
   class Survey
     attr_reader :name
-    attr_accessor :questions
+    attr_accessor :questions, :responses
 
     def initialize(attributes = {})
       @name = attributes[:name]
       @questions = []
+      @responses = []
     end
 
     def add_question(question)
       @questions << question
+    end
+    
+    def add_response(response)
+      @responses << response
     end
   end
 end
