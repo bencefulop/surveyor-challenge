5fb2cf781454b3c17fbf6da53a56be71733ba03b
Bence Fulop <fbence90@gmail.com>
2020-10-18 18:36:33 +1100

    add new classes to module as required

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -2,7 +2,10 @@ require "surveyor/question"
 require "surveyor/free_text_question"
 require "surveyor/rating_question"
 require "surveyor/survey"
+require "surveyor/answer"
+require "surveyor/response"
 require "surveyor/version"
+
 
 module Surveyor
 end
