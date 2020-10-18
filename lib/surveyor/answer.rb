bd1fb506d34953341355bd344221002abaf878af
Bence Fulop <fbence90@gmail.com>
2020-10-18 18:33:05 +1100

    Add find response by email method to survey

--- lib/surveyor/answer.rb
+++ lib/surveyor/answer.rb
@@ -2,11 +2,16 @@ require_relative 'response'
 
 module Surveyor
   class Answer
-    attr_accessor :question
+    attr_accessor :question, :value
 
     def initialize(question, *value)
-      @question = question
+      @question = valid_question?(question)
       @value = value
+    end
+
+    def valid_question?(question)
+      question_error = StandardError.new "Must specify the question you're answering"
+      question.class == String && question[-1] == "?" ? question : question_error
     end
   end
 end
